f3316aa754f7b5bb82f8c952434260e817789188
chaoa_wang <1435768264@qq.com>
2020-12-26 10:11:34 +0800

    删除项目

--- JMmes/src/main/java/com/bluebirdme/mes/linglong/wsdl/mixserviceex/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "dataobjects.common.plantops.datasweep.com")
-package com.bluebirdme.mes.linglong.wsdl.mixserviceex;
-
--- JMmes/src/main/java/com/print/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.print.com/")
-package com.print.client;
